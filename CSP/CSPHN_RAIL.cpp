748e8e5f0e6ad6007b1f36d74fa32ad7c788806f
Defy logic guy <ngtuanminh961@gmail.com>
2025-03-02 11:07:03 +0700

    CSP

---
+++ CSP/CSPHN_RAIL.cpp
@@ -0,0 +1,46 @@
+/*************************
+  Author: Defy logic guy
+  16:03:24 - 01/03/2025
+*************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define float long double
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define heap priority_queue
+#define pb push_back
+#define MOD 1000000007
+#define endl '\n'
+#define NAME "CSPHN_RAIL"
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    
+}
+
+signed main()
+{
+    if (fopen(NAME ".INP", "r"))
+    {
+        freopen(NAME ".INP", "r", stdin);
+        freopen(NAME ".OUT", "w", stdout);
+    }
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int tt = 1;
+    // cin >> tt;
+
+    while (tt--)
+        solve();
+    return 0;
+}
+    
