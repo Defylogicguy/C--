37ec47c312600a48d90ad140fddedb571dc19e74
Defy logic guy <ngtuanminh961@gmail.com>
2025-06-23 14:18:04 +0700

    feat: create ABC059B.cpp, ABC062B.cpp and ABC139C.cpp

---
+++ Atcoder/ABC059B.cpp
@@ -0,0 +1,65 @@
+/*************************
+  Author: Defy logic guy
+  17:39:26 - 22/06/2025
+*************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define float double
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define maxall(x) *max_element(all(x))
+#define minall(x) *min_element(all(x))
+#define heap priority_queue
+#define pb push_back
+#define MOD 1000000007
+#define NAME "ABC059B"
+
+void solve()
+{
+    string a, b;
+    cin >> a >> b;
+    if (a.size() < b.size())
+    {
+        cout << "LESS";
+        return;
+    }
+    else if (a.size() > b.size())
+    {
+        cout << "GREATER";
+        return;
+    }
+    int n = a.size();
+    for (int i = 0; i < n; i++)
+        if (a[i] < b[i])
+        {
+            cout << "LESS";
+            return;
+        }
+        else if (a[i] > b[i])
+        {
+            cout << "GREATER";
+            return;
+        }
+    cout << "EQUAL";
+}
+
+signed main()
+{
+    if (fopen(NAME ".INP", "r"))
+    {
+        freopen(NAME ".INP", "r", stdin);
+        freopen(NAME ".OUT", "w", stdout);
+    }
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int tt = 1;
+    // cin >> tt;
+
+    while (tt--)
+        solve();
+    return 0;
+}
