fa8b0308a2053d09dfa8a4711850cd14c600518e
Defy logic guy <ngtuanminh961@gmail.com>
2025-09-18 21:27:41 +0700

    create 7 files

---
+++ Codeforces/2139A.cpp
@@ -0,0 +1,59 @@
+/*************************
+  Author: Defy logic guy
+  20:56:56 - 18/09/2025
+*************************/
+#include <bits/stdc++.h>
+using namespace std;
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << "(" << p.first << ", " << p.second << ")"; }
+template <typename T>
+auto operator<<(ostream &os, const T &c) -> decltype(c.begin(), c.end(), os)
+{
+    for (auto it = c.begin(); it != c.end(); ++it)
+        os << (it == c.begin() ? "{" : ", ") << *it;
+    return os << "}";
+}
+#define dbg(x) cerr << #x << " = " << (x) << '\n';
+#define int long long
+#define float double
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define maxall(x) *max_element(all(x))
+#define minall(x) *min_element(all(x))
+#define heap priority_queue
+#define pb push_back
+#define MOD 1000000007
+#define NAME "2139A"
+
+void solve()
+{
+    int a, b;
+    cin >> a >> b;
+    if (a == b )
+        cout << 0 << '\n';
+    else if (b % a == 0 or a % b == 0)
+        cout << 1 << '\n';
+    else
+        cout << 2 << '\n';
+}
+
+signed main()
+{
+    if (fopen(NAME ".INP", "r"))
+    {
+        freopen(NAME ".INP", "r", stdin);
+        freopen(NAME ".OUT", "w", stdout);
+    }
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int tt = 1;
+    cin >> tt;
+
+    while (tt--)
+        solve();
+
+    return 0;
+}
---
+++ Codeforces/2139B.cpp
@@ -0,0 +1,61 @@
+/*************************
+  Author: Defy logic guy
+  21:22:40 - 18/09/2025
+*************************/
+#include <bits/stdc++.h>
+using namespace std;
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << "(" << p.first << ", " << p.second << ")"; }
+template <typename T>
+auto operator<<(ostream &os, const T &c) -> decltype(c.begin(), c.end(), os)
+{
+    for (auto it = c.begin(); it != c.end(); ++it)
+        os << (it == c.begin() ? "{" : ", ") << *it;
+    return os << "}";
+}
+#define dbg(x) cerr << #x << " = " << (x) << '\n';
+#define int long long
+#define float double
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define maxall(x) *max_element(all(x))
+#define minall(x) *min_element(all(x))
+#define heap priority_queue
+#define pb push_back
+#define MOD 1000000007
+#define NAME "2139B"
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    int ans = 0 ;
+    sort(rall(a));
+    for (int i = 0; i < n; i++)
+        ans += a[i] * max(0ll, m - i);
+    cout << ans << '\n';
+}
+
+signed main()
+{
+    if (fopen(NAME ".INP", "r"))
+    {
+        freopen(NAME ".INP", "r", stdin);
+        freopen(NAME ".OUT", "w", stdout);
+    }
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int tt = 1;
+    cin >> tt;
+
+    while (tt--)
+        solve();
+
+    return 0;
+}
