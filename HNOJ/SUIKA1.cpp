721c76aedcfe7c6dbc1e26a2a578b6da82633298
Defy logic guy <ngtuanminh961@gmail.com>
2025-11-17 23:42:21 +0700

    create 9 files

---
+++ HNOJ/SUIKA1.cpp
@@ -0,0 +1,82 @@
+/*************************
+  Author: Defy logic guy
+  22:54:28 - 14/11/2025
+*************************/
+#include <bits/stdc++.h>
+using namespace std;
+ostream &operator<<(ostream &os, const string &s)
+{
+    os.write(s.data(), s.size());
+    return os;
+}
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << "(" << p.first << ", " << p.second << ")"; }
+template <typename T>
+auto operator<<(ostream &os, const T &c) -> typename enable_if<!is_same<T, string>::value, decltype(c.begin(), c.end(), os)>::type
+{
+    for (auto it = c.begin(); it != c.end(); ++it)
+        os << (it == c.begin() ? "{" : ", ") << *it;
+    return os << "}";
+}
+#define dbg(x) cerr << #x << " = " << (x) << '\n';
+#define int long long
+#define float double
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define maxall(x) *max_element(all(x))
+#define minall(x) *min_element(all(x))
+#define heap priority_queue
+#define pb push_back
+#define MOD 1000000007
+#define NAME "SUIKA1"
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n + 1);
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+    vector<int> prv(n + 1, 0);
+    map<int, int> mp;
+    for (int i = 1; i <= n; i++)
+    {
+        if (mp.count(a[i]))
+            prv[i] = mp[a[i]];
+        mp[a[i]] = i;
+    }
+
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+    for (int i = 0; i <= n; i++)
+        dp[i][0] = 1;
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= m; j++)
+        {
+            int plus = dp[i - 1][j - 1];
+            if (prv[i] != 0)
+                (plus -= dp[prv[i] - 1][j - 1]) %= MOD;
+            dp[i][j] = (dp[i - 1][j] + plus + MOD) % MOD;
+        }
+    cout << dp[n][m];
+}
+
+signed main()
+{
+    if (fopen(NAME ".INP", "r"))
+    {
+        freopen(NAME ".INP", "r", stdin);
+        freopen(NAME ".OUT", "w", stdout);
+    }
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int tt = 1;
+    // cin >> tt;
+
+    while (tt--)
+        solve();
+
+    return 0;
+}
---
+++ VNOI/PBCDEM.cpp
@@ -0,0 +1,65 @@
+/*************************
+  Author: Defy logic guy
+  20:34:55 - 12/11/2025
+*************************/
+#include <bits/stdc++.h>
+using namespace std;
+ostream &operator<<(ostream &os, const string &s)
+{
+    os.write(s.data(), s.size());
+    return os;
+}
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << "(" << p.first << ", " << p.second << ")"; }
+template <typename T>
+auto operator<<(ostream &os, const T &c) -> typename enable_if<!is_same<T, string>::value, decltype(c.begin(), c.end(), os)>::type
+{
+    for (auto it = c.begin(); it != c.end(); ++it)
+        os << (it == c.begin() ? "{" : ", ") << *it;
+    return os << "}";
+}
+#define dbg(x) cerr << #x << " = " << (x) << '\n';
+#define int long long
+#define float double
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define maxall(x) *max_element(all(x))
+#define minall(x) *min_element(all(x))
+#define heap priority_queue
+#define pb push_back
+#define MOD 1000000007
+#define NAME "PBCDEM"
+
+void solve()
+{
+    // Cannot AC cuz the problem needs bignum
+    int n;
+    cin >> n;
+    vector<int> dp(n + 1, 0);
+    dp[0] = 1;
+    for (int i = 1; i <= n; i++)
+        for (int j = n; j >= i; j--)
+            dp[j] += dp[j - i];
+    cout << dp[n] - 1;
+}
+
+signed main()
+{
+    if (fopen(NAME ".INP", "r"))
+    {
+        freopen(NAME ".INP", "r", stdin);
+        freopen(NAME ".OUT", "w", stdout);
+    }
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int tt = 1;
+    // cin >> tt;
+
+    while (tt--)
+        solve();
+
+    return 0;
+}
