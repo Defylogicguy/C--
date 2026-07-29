8a811b51223cc90006864b3af9727d06ab901d59
Defylogicguy <ngtuanminh961@gmail.com>
2026-07-29 23:15:41 +0700

    update 914 files and delete 1 file

--- .Trash-1000/files/.idk.cpp
+++ .Trash-1000/files/.idk.cpp
@@ -1,6 +1,6 @@
 /*************************
   Author: Defy logic guy
-  21:55:23 - 28/07/2026
+  22:38:46 - 29/07/2026
 *************************/
 #include <bits/stdc++.h>
 using namespace std;
@@ -27,47 +27,18 @@ auto operator<<(ostream &os, const T &c) -> typename enable_if<!is_same<T, strin
 #define heap priority_queue
 #define pb push_back
 #define MOD 1000000007
-#define NAME "idk"
+#define NAME ".idk"
 
 void solve()
 {
     int n;
     cin >> n;
-    vector<vector<int>> adj(n + 1);
-    for (int i = 1; i < n; i++)
+    for (int i = 0, x; i < n; i++)
     {
-        int u, v;
-        cin >> u >> v;
-        adj[u].pb(v),adj[v].pb(u);
+        cin >> x;
+        cout << x;
     }
-    auto bfs = [&](int x)
-    {
-        vector<int> d(n + 1, -1);
-        queue<int> q;
-        q.push(x);
-        d[x] = 0;
-        while (q.size())
-        {
-            int u = q.front();
-            q.pop();
-            for (int v : adj[u])
-                if (d[v] == -1)
-                {
-                    d[v] = d[u] + 1;
-                    q.push(v);
-                }
-        }
-        int t = x;
-        for (int i = 1; i <= n; i++)
-            if (d[i] > d[t])
-                t = i;
-        return make_pair(t, d);
-    };
-    auto [a, _] = bfs(1);
-    auto [b, da] = bfs(a);
-    auto [__, db] = bfs(b);
-    for (int i = 1; i <= n; i++)
-        cout << max(da[b], max(da[i], db[i]) + 1) << '\n';
+    cout << '\n';
 }
 
 signed main()
